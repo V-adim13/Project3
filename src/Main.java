c034ff5eb80f1a4965d1eb005ce612e8ddc8a9ea
*V-adim13* <pogopogo21696@gmail.com>
2022-06-14 17:41:48 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int score = 100;
+
+        int add = 1650;
+
+        int bonus = 0;
+        if (add > 1000) {
+
+            bonus = add / 100;
+
+            int end = score + add + bonus;
+
+            System.out.println("Итоговый счет:" + end);
+            System.out.printf("Бонус: " + bonus);
+        }
+
+
+    }
+}
